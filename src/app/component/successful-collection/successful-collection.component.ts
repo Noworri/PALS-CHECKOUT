7ad51bfb4003473acbc4c391c0d8bb77278dd1b2
E2dwhy <eddyyoann@gmail.com>
2022-10-09 13:37:42 +0000

    fix: update support for successful collection

--- src/app/component/successful-collection/successful-collection.component.ts
+++ src/app/component/successful-collection/successful-collection.component.ts
@@ -4,6 +4,7 @@ import {
   TRANSFER_DATA_KEY,
   BUSINESS_DATA_KEY,
 } from 'src/app/constant/constants';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-successful-collection',
@@ -11,27 +12,31 @@ import {
   styleUrls: ['./successful-collection.component.scss'],
 })
 export class SuccessfulCollectionComponent implements OnInit {
-  transferData: any;
+  businessTransactionData: any;
   collectionData: any;
   businessData: any;
 
-  constructor() {
+  constructor(private location: Location) {
     const collection = sessionStorage.getItem(COLLECTION_DATA_KEY);
     this.collectionData =
       collection === null ? undefined : JSON.parse(collection);
 
     const transfer = sessionStorage.getItem(TRANSFER_DATA_KEY);
-    this.transferData = transfer === null ? undefined : JSON.parse(transfer);
+    this.businessTransactionData = transfer === null ? undefined : JSON.parse(transfer);
     const sessionData = sessionStorage.getItem(BUSINESS_DATA_KEY);
     this.businessData =
       sessionData === null ? undefined : JSON.parse(sessionData);
   }
 
   ngOnInit() {
-    if (this.transferData.callback_url) {
+    if (this.businessTransactionData.callback_url) {
       setTimeout(() => {
-        window.location.href = this.transferData.callback_url;
+        window.location.href = this.businessTransactionData.callback_url;
       }, 5000);
     }
+  }
+
+  onRetryPayement() {
+    this.location.back();
   }
 }
