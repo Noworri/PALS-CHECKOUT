16663e682493a61dafefb162f4bc1135dcea18ad
E2dwhy <eddyyoann@gmail.com>
2022-10-25 11:11:44 +0000

    fix: use reference for status verification allow

--- src/app/component/allow-cash-out/allow-cash-out.component.ts
+++ src/app/component/allow-cash-out/allow-cash-out.component.ts
@@ -81,7 +81,7 @@ export class AllowCashOutComponent implements OnInit, OnDestroy {
       .subscribe(
         (response) => {
           if (response && response['status'] === true) {
-            const collection = {...this.collectionData, reference: response['reference']};
+            const collection = {...this.collectionData, reference: response['data']['reference']};
             sessionStorage.setItem(COLLECTION_DATA_KEY, JSON.stringify(collection));
             this.router.navigate(['/mtn']);
             // if (!this.businessTransactionData.callback_url) {
